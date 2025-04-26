bc528ba42bcb1c0c97f8d37f8bac39896d53b4c9
panuwit009 <tinsologuitar@gmail.com>
2025-04-26 22:09:13 +0700

    add router get id

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -4,8 +4,30 @@ import pool from '../db';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const result = await pool.query('SELECT * FROM posts');
-  res.json(result.rows);
+  try {
+    const result = await pool.query('SELECT * FROM posts');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error'});
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query('SELECT * FROM posts WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error'});
+  }
 });
 
 router.post('/', async (req, res) => {
